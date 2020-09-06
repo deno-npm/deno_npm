915b822af688b5d41d008c0a86a6d6c3083be5e1
Nicolas Guerrero <stephenguerrero43@gmail.com>
2020-09-05 19:49:23 -0500

    Added fully functional and tested ethjs-sha3 library

---
+++ ethjs-sha3/0.1.3/deps.js
@@ -0,0 +1,3 @@
+export { default as BigNumber } from "https://cdn.skypack.dev/bignumber.js@9.0.0";
+export { default as Buffer } from "https://cdn.skypack.dev/buffer@5.6.0";
+export { default as jssha3 } from "https://cdn.skypack.dev/js-sha3@0.5.5";
